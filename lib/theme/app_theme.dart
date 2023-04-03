7886a0af9594686cd12fe49511b61a91f86c045d
lydiasama <hallelujah.lydia@gmail.com>
2023-04-03 17:14:08 +0700

    [#7] Fix format issue

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,31 +4,30 @@ import '../gen/assets.gen.dart';
 
 class AppTheme {
   static ThemeData get defaultTheme => ThemeData(
-      primarySwatch: Colors.blue,
-      primaryColor: Colors.white,
-      brightness: Brightness.light,
-      fontFamily: Assets.fonts.neuzeitRegular,
-      textTheme: const TextTheme(
-        titleLarge: TextStyle(
-          fontSize: 34.0,
-          fontWeight: FontWeight.w800,
-          color: Colors.white,
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        brightness: Brightness.light,
+        fontFamily: Assets.fonts.neuzeitRegular,
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 34.0,
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+          ),
+          titleMedium: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+          ),
+          titleSmall: TextStyle(
+            fontSize: 17.0,
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 17.0,
+            color: Colors.white,
+          ),
         ),
-        titleMedium: TextStyle(
-          fontSize: 28.0,
-          fontWeight: FontWeight.w800,
-          color: Colors.white,
-        ),
-        titleSmall: TextStyle(
-          fontSize: 17.0,
-          fontWeight: FontWeight.w800,
-          color: Colors.white,
-        ),
-        bodyMedium: TextStyle(
-          fontSize: 17.0,
-          color: Colors.white,
-        ),
-      ),
-    );
-
+      );
 }
