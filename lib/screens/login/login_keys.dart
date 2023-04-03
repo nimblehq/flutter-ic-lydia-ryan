4f113302681f429896808bc5477da70e00a93f18
lydiasama <hallelujah.lydia@gmail.com>
2023-04-03 17:29:37 +0700

    [#7] Remove unused code

--- lib/screens/login/login_keys.dart
+++ lib/screens/login/login_keys.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class LoginKey {
-  LoginKey._();
-
   static const liLoginEmail = Key('liLoginEmail');
   static const liLoginPassword = Key('liLoginPassword');
   static const rrbLogin = Key('rrbLogin');
