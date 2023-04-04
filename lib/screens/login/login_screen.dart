c6675dd8f3cb9257c64c799b7e690a77ba2787fe
lydiasama <hallelujah.lydia@gmail.com>
2023-04-04 18:17:21 +0700

    [#7] Refactor layout

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -16,67 +16,63 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Stack(
-        children: [
-          Container(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Assets.images.bgLogin.path),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 25.0, sigmaY: 25.0),
+          child: Container(
             decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(Assets.images.bgLogin.path),
-                fit: BoxFit.cover,
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.black,
+                  Colors.black.withOpacity(0.20),
+                ],
               ),
             ),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 25.0, sigmaY: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                    colors: [
-                      Colors.black,
-                      Colors.black.withOpacity(0.20),
-                    ],
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: Dimensions.paddingDefault),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SvgPicture.asset(
-                          Assets.images.nimbleLogoWhite,
-                        ),
-                        const SizedBox(height: 109),
-                        const LoginInputWidget(
-                          key: LoginKey.liLoginEmail,
-                          hintText: 'Email',
-                          keyboardType: TextInputType.emailAddress,
-                          textInputAction: TextInputAction.next,
-                        ),
-                        const SizedBox(height: Dimensions.paddingMedium),
-                        const LoginInputWidget(
-                          key: LoginKey.liLoginPassword,
-                          hintText: 'Password',
-                          isPasswordType: true,
-                        ),
-                        const SizedBox(height: Dimensions.paddingMedium),
-                        RoundedRectangleButtonWidget(
-                          key: LoginKey.rrbLogin,
-                          text: 'Login',
-                          onPressed: () {
-                            // TODO: Login
-                          },
-                        ),
-                      ],
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: Dimensions.paddingDefault),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      Assets.images.nimbleLogoWhite,
                     ),
-                  ),
+                    const SizedBox(height: 109),
+                    const LoginInputWidget(
+                      key: LoginKey.liLoginEmail,
+                      hintText: 'Email',
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.next,
+                    ),
+                    const SizedBox(height: Dimensions.paddingMedium),
+                    const LoginInputWidget(
+                      key: LoginKey.liLoginPassword,
+                      hintText: 'Password',
+                      isPasswordType: true,
+                    ),
+                    const SizedBox(height: Dimensions.paddingMedium),
+                    RoundedRectangleButtonWidget(
+                      key: LoginKey.rrbLogin,
+                      text: 'Login',
+                      onPressed: () {
+                        // TODO: Login
+                      },
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
