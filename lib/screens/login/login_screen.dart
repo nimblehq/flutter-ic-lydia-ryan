8c4f06f26d298eb93a38f43d96fefb1230c21097
lydiasama <hallelujah.lydia@gmail.com>
2023-04-04 18:08:22 +0700

    [#7] Update blurry and dimmed background

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:lydiaryanfluttersurvey/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -12,50 +14,69 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(Assets.images.bgLogin.path),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Padding(
-          padding:
-              const EdgeInsets.symmetric(horizontal: Dimensions.paddingDefault),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  Assets.images.nimbleLogoWhite,
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(Assets.images.bgLogin.path),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 25.0, sigmaY: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.black,
+                      Colors.black.withOpacity(0.20),
+                    ],
+                  ),
                 ),
-                const SizedBox(height: 109),
-                const LoginInputWidget(
-                  key: LoginKey.liLoginEmail,
-                  hintText: 'Email',
-                  keyboardType: TextInputType.emailAddress,
-                  textInputAction: TextInputAction.next,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: Dimensions.paddingDefault),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          Assets.images.nimbleLogoWhite,
+                        ),
+                        const SizedBox(height: 109),
+                        const LoginInputWidget(
+                          key: LoginKey.liLoginEmail,
+                          hintText: 'Email',
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                        ),
+                        const SizedBox(height: Dimensions.paddingMedium),
+                        const LoginInputWidget(
+                          key: LoginKey.liLoginPassword,
+                          hintText: 'Password',
+                          isPasswordType: true,
+                        ),
+                        const SizedBox(height: Dimensions.paddingMedium),
+                        RoundedRectangleButtonWidget(
+                          key: LoginKey.rrbLogin,
+                          text: 'Login',
+                          onPressed: () {
+                            // TODO: Login
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                const SizedBox(height: Dimensions.paddingMedium),
-                const LoginInputWidget(
-                  key: LoginKey.liLoginPassword,
-                  hintText: 'Password',
-                  isPasswordType: true,
-                ),
-                const SizedBox(height: Dimensions.paddingMedium),
-                RoundedRectangleButtonWidget(
-                  key: LoginKey.rrbLogin,
-                  text: 'Login',
-                  onPressed: () {
-                    // TODO: Login
-                  },
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
