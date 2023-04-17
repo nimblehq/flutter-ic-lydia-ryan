bad1a37e74a83965de472e972c16a95faf35be88
lydiasama <hallelujah.lydia@gmail.com>
2023-04-17 21:10:40 +0700

    [#9] Call Toast message with async

--- lib/utils/toast_message.dart
+++ lib/utils/toast_message.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-void showToast(String message) {
-  Fluttertoast.cancel();
-  Fluttertoast.showToast(
+void showToast(String message) async {
+  await Fluttertoast.cancel();
+  await Fluttertoast.showToast(
     msg: message,
     toastLength: Toast.LENGTH_SHORT,
     gravity: ToastGravity.BOTTOM,
