21d2bc4044822c7cf1126b02cb87e3dcdf1350f3
lydiasama <hallelujah.lydia@gmail.com>
2023-04-19 11:24:54 +0700

    [#43] Save accessToken and refreshToken after logging in success

--- lib/usecases/login_use_case.dart
+++ lib/usecases/login_use_case.dart
@@ -3,6 +3,8 @@ import 'package:lydiaryanfluttersurvey/api/repository/auth_repository.dart';
 import 'package:lydiaryanfluttersurvey/model/response/login_response.dart';
 import 'package:lydiaryanfluttersurvey/usecases/base/base_use_case.dart';
 
+import '../database/shared_preferences_utils.dart';
+
 class LoginInput {
   final String email;
   final String password;
@@ -16,16 +18,22 @@ class LoginInput {
 @Injectable()
 class LoginUseCase extends UseCase<LoginResponse, LoginInput> {
   final AuthRepository _authRepository;
+  final SharedPreferencesUtils _sharedPreferenceUtils;
 
-  LoginUseCase(this._authRepository);
+  LoginUseCase(this._authRepository, this._sharedPreferenceUtils);
 
   @override
   Future<Result<LoginResponse>> call(LoginInput params) {
-    return _authRepository
-        .login(params.email, params.password)
-        // ignore: unnecessary_cast
-        .then((value) => Success(value) as Result<LoginResponse>)
-        .onError<Exception>(
-            (error, stackTrace) => Failed(UseCaseException(error)));
+    return _authRepository.login(params.email, params.password).then((value) {
+      _saveToken(value);
+      // ignore: unnecessary_cast
+      return Success(value) as Result<LoginResponse>;
+    }).onError<Exception>(
+        (error, stackTrace) => Failed(UseCaseException(error)));
+  }
+
+  void _saveToken(LoginResponse loginResponse) {
+    _sharedPreferenceUtils.setAccessToken(loginResponse.accessToken);
+    _sharedPreferenceUtils.setRefreshToken(loginResponse.refreshToken);
   }
 }
--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:lydiaryanfluttersurvey/api/api_service.dart';
 import 'package:lydiaryanfluttersurvey/api/repository/auth_repository.dart';
 import 'package:lydiaryanfluttersurvey/api/service/auth_service.dart';
+import 'package:lydiaryanfluttersurvey/database/shared_preferences_utils.dart';
 import 'package:lydiaryanfluttersurvey/usecases/base/base_use_case.dart';
 import 'package:lydiaryanfluttersurvey/usecases/login_use_case.dart';
 import 'package:mockito/annotations.dart';
@@ -13,6 +14,7 @@ import 'package:mockito/annotations.dart';
   MockSpec<DioError>(),
   MockSpec<UseCaseException>(),
   MockSpec<LoginUseCase>(),
+  MockSpec<SharedPreferencesUtils>(),
 ])
 main() {
   // empty class to generate mock repository classes
