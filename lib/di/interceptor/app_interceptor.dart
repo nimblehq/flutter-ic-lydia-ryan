75f11638329a686b6d3831b9d68416b61cda8e1b
lydiasama <hallelujah.lydia@gmail.com>
2023-04-20 10:33:35 +0700

    [#43] Implement interceptor to refresh token

--- lib/di/interceptor/app_interceptor.dart
+++ lib/di/interceptor/app_interceptor.dart
@@ -1,23 +1,30 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:lydiaryanfluttersurvey/di/injection.dart';
+import 'package:lydiaryanfluttersurvey/model/response/login_response.dart';
+import 'package:lydiaryanfluttersurvey/storage/shared_preferences_utils.dart';
+import 'package:lydiaryanfluttersurvey/usecases/base/base_use_case.dart';
+import 'package:lydiaryanfluttersurvey/usecases/refresh_token_use_case.dart';
+
+const _headerAuthorization = 'Authorization';
 
 class AppInterceptor extends Interceptor {
   final bool _requireAuthenticate;
   final Dio _dio;
+  final SharedPreferencesUtils _sharedPreferencesUtils;
 
   AppInterceptor(
     this._requireAuthenticate,
     this._dio,
+    this._sharedPreferencesUtils,
   );
 
   @override
   Future onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
     if (_requireAuthenticate) {
-      // TODO header authorization here
-      // options.headers
-      //     .putIfAbsent(HEADER_AUTHORIZATION, () => "");
+      options.headers.putIfAbsent(_headerAuthorization, () => "");
     }
     return super.onRequest(options, handler);
   }
@@ -35,34 +42,37 @@ class AppInterceptor extends Interceptor {
   }
 
   Future<void> _doRefreshToken(
-    DioError err,
+    DioError error,
     ErrorInterceptorHandler handler,
   ) async {
     try {
-      // TODO Request new token
+      final RefreshTokenUseCase refreshTokenUseCase =
+          getIt<RefreshTokenUseCase>();
+      final result = await refreshTokenUseCase.call();
+      if (result is Success<LoginResponse>) {
+        // Update new token header
+        final newAuthToken =
+            result.value.tokenType + _sharedPreferencesUtils.accessToken;
+        error.requestOptions.headers[_headerAuthorization] = newAuthToken;
 
-      // if (result is Success) {
-      // TODO Update new token header
-      // err.requestOptions.headers[_headerAuthorization] = newToken;
-
-      // Create request with new access token
-      final options = Options(
-          method: err.requestOptions.method,
-          headers: err.requestOptions.headers);
-      final newRequest = await _dio.request(
-          "${err.requestOptions.baseUrl}${err.requestOptions.path}",
-          options: options,
-          data: err.requestOptions.data,
-          queryParameters: err.requestOptions.queryParameters);
-      handler.resolve(newRequest);
-      //  } else {
-      //    handler.next(err);
-      //  }
+        // Create request with new access token
+        final options = Options(
+            method: error.requestOptions.method,
+            headers: error.requestOptions.headers);
+        final newRequest = await _dio.request(
+            "${error.requestOptions.baseUrl}${error.requestOptions.path}",
+            options: options,
+            data: error.requestOptions.data,
+            queryParameters: error.requestOptions.queryParameters);
+        handler.resolve(newRequest);
+      } else {
+        handler.next(error);
+      }
     } catch (exception) {
       if (exception is DioError) {
         handler.next(exception);
       } else {
-        handler.next(err);
+        handler.next(error);
       }
     }
   }
