40309361eecb36ab9692fb9d2d6536423df4dfd6
lydiasama <hallelujah.lydia@gmail.com>
2023-04-20 19:34:13 +0700

    [#16] Fix dependency injection and generate mock

--- lib/di/module/service_module.dart
+++ lib/di/module/service_module.dart
@@ -1,5 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:lydiaryanfluttersurvey/api/service/auth_service.dart';
+import 'package:lydiaryanfluttersurvey/api/service/survey_service.dart';
 import 'package:lydiaryanfluttersurvey/di/provider/dio_provider.dart';
 import 'package:lydiaryanfluttersurvey/env.dart';
 
@@ -8,4 +9,8 @@ abstract class ServiceModule {
   @Singleton()
   AuthService provideAuthService(DioProvider dioProvider) =>
       AuthService(dioProvider.getDio(), baseUrl: Env.restApiEndpoint);
+
+  @Singleton()
+  SurveyService provideSurveyService(DioProvider dioProvider) =>
+      SurveyService(dioProvider.getDio(), baseUrl: Env.restApiEndpoint);
 }
--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -1,9 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:lydiaryanfluttersurvey/api/api_service.dart';
 import 'package:lydiaryanfluttersurvey/api/repository/auth_repository.dart';
+import 'package:lydiaryanfluttersurvey/api/repository/survey_repository.dart';
 import 'package:lydiaryanfluttersurvey/api/service/auth_service.dart';
+import 'package:lydiaryanfluttersurvey/api/service/survey_service.dart';
 import 'package:lydiaryanfluttersurvey/storage/shared_preferences_utils.dart';
 import 'package:lydiaryanfluttersurvey/usecases/base/base_use_case.dart';
+import 'package:lydiaryanfluttersurvey/usecases/get_surveys_use_case.dart';
 import 'package:lydiaryanfluttersurvey/usecases/login_use_case.dart';
 import 'package:lydiaryanfluttersurvey/usecases/verify_logged_in_use_case.dart';
 import 'package:mockito/annotations.dart';
@@ -11,12 +14,15 @@ import 'package:mockito/annotations.dart';
 @GenerateNiceMocks([
   MockSpec<ApiService>(),
   MockSpec<AuthRepository>(),
+  MockSpec<SurveyRepository>(),
   MockSpec<AuthService>(),
+  MockSpec<SurveyService>(),
   MockSpec<DioError>(),
   MockSpec<SharedPreferencesUtils>(),
   MockSpec<UseCaseException>(),
   MockSpec<LoginUseCase>(),
   MockSpec<VerifyLoggedInUseCase>(),
+  MockSpec<GetSurveysUseCase>(),
 ])
 main() {
   // empty class to generate mock repository classes
