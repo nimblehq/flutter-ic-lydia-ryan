6085114be48f4a27be6a4097dc28a2a714aec6a9
Ryan Conway <ryan@nimblehq.co>
2023-04-21 16:27:04 +0700

    [#15] Add page indicator to survey list

--- lib/screens/home/widget/home_paging_widget.dart
+++ lib/screens/home/widget/home_paging_widget.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:lydiaryanfluttersurvey/model/ui/survey_ui_model.dart';
 import 'package:lydiaryanfluttersurvey/screens/common/background_widget.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomePagingWidget extends StatefulWidget {
   final List<SurveyUiModel> surveys;
@@ -36,21 +37,29 @@ class _HomePagingWidgetState extends State<HomePagingWidget> {
                   ))
               .toList(),
         ),
-        _buildSurveyItem(context, widget.surveys[_currentIndex])
+        _buildSurveyItem(context, widget.surveys[_currentIndex], pageController)
       ],
     );
   }
 
-  Widget _buildSurveyItem(BuildContext context, SurveyUiModel survey) {
+  Widget _buildSurveyItem(
+    BuildContext context,
+    SurveyUiModel survey,
+    PageController controller,
+  ) {
     return Stack(
       children: [
         SafeArea(
-          child: Column(
-            children: [
-              _buildSurveyHeader(context, survey),
-              const Spacer(),
-              _buildSurveyFooter(context, survey),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildSurveyHeader(context, survey),
+                const Spacer(),
+                _buildSurveyFooter(context, survey, controller),
+              ],
+            ),
           ),
         )
       ],
@@ -58,31 +67,26 @@ class _HomePagingWidgetState extends State<HomePagingWidget> {
   }
 
   Widget _buildSurveyHeader(BuildContext context, SurveyUiModel survey) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                DateFormat("EEEE, MMMM dd")
-                    .format(survey.activeAt)
-                    .toUpperCase(),
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge
-                    ?.copyWith(fontSize: 13),
-              ),
-              Text(
-                _daysSince(context, survey.activeAt),
-                style: Theme.of(context).textTheme.titleLarge,
-              )
-            ],
-            //TODO add user avatar
-          ),
-        ],
-      ),
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              DateFormat("EEEE, MMMM dd").format(survey.activeAt).toUpperCase(),
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(fontSize: 13),
+            ),
+            Text(
+              _daysSince(context, survey.activeAt),
+              style: Theme.of(context).textTheme.titleLarge,
+            )
+          ],
+          //TODO add user avatar
+        ),
+      ],
     );
   }
 
@@ -100,38 +104,51 @@ class _HomePagingWidgetState extends State<HomePagingWidget> {
     }
   }
 
-  Widget _buildSurveyFooter(BuildContext context, SurveyUiModel survey) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //TODO add page indicator?
-          Text(survey.title,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.titleMedium),
-          Row(
-            children: [
-              Expanded(
-                  child: Text(survey.description,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.bodyMedium)),
-              Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: FloatingActionButton(
-                      backgroundColor: Theme.of(context).primaryColor,
-                      onPressed: () => {},
-                      child: const Icon(
-                        Icons.chevron_right,
-                        color: Colors.black,
-                        size: 56 * 0.75, // 75% of fab size
-                      )))
-            ],
-          )
-        ],
-      ),
+  Widget _buildSurveyFooter(
+    BuildContext context,
+    SurveyUiModel survey,
+    PageController controller,
+  ) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: SmoothPageIndicator(
+            controller: controller,
+            count: widget.surveys.length,
+            effect: const ScrollingDotsEffect(
+              dotColor: Colors.white24,
+              activeDotColor: Colors.white,
+              dotHeight: 8,
+              dotWidth: 8,
+            ),
+          ),
+        ),
+        Text(survey.title,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.titleMedium),
+        Row(
+          children: [
+            Expanded(
+                child: Text(survey.description,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.bodyMedium)),
+            Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: FloatingActionButton(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    onPressed: () => {},
+                    child: const Icon(
+                      Icons.chevron_right,
+                      color: Colors.black,
+                      size: 56 * 0.75, // 75% of fab size
+                    )))
+          ],
+        )
+      ],
     );
   }
 }
