e41af4cb30cd495e39f743fa4988d446f090cfce
lydiasama <hallelujah.lydia@gmail.com>
2023-04-24 17:00:01 +0700

    [#18] Remove unused use case

--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -8,7 +8,6 @@ import 'package:lydiaryanfluttersurvey/storage/shared_preferences_utils.dart';
 import 'package:lydiaryanfluttersurvey/usecases/base/base_use_case.dart';
 import 'package:lydiaryanfluttersurvey/usecases/get_surveys_use_case.dart';
 import 'package:lydiaryanfluttersurvey/usecases/login_use_case.dart';
-import 'package:lydiaryanfluttersurvey/usecases/verify_logged_in_use_case.dart';
 import 'package:mockito/annotations.dart';
 
 @GenerateNiceMocks([
@@ -21,7 +20,6 @@ import 'package:mockito/annotations.dart';
   MockSpec<SharedPreferencesUtils>(),
   MockSpec<UseCaseException>(),
   MockSpec<LoginUseCase>(),
-  MockSpec<VerifyLoggedInUseCase>(),
   MockSpec<GetSurveysUseCase>(),
 ])
 main() {
