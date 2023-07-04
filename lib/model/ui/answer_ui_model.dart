ce465f1898117fc25d5d5227f6971f4e031a2468
lydiasama <hallelujah.lydia@gmail.com>
2023-07-04 16:07:51 +0700

    [#31] Remove unused property

--- lib/model/ui/answer_ui_model.dart
+++ lib/model/ui/answer_ui_model.dart
@@ -3,12 +3,10 @@ import 'package:lydiaryanfluttersurvey/model/response/answer_response.dart';
 class AnswerUiModel {
   final String id;
   final String? text;
-  final String? inputMaskPlaceholder;
 
   AnswerUiModel({
     required this.id,
     required this.text,
-    this.inputMaskPlaceholder,
   });
 
   factory AnswerUiModel.fromAnswerResponse(AnswerResponse e) {
