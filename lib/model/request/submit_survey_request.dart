55e6acc61c4bfdf9b96b54abeaaf3431243af4fa
Ryan Conway <ryan@nimblehq.co>
2023-07-20 15:30:38 +0700

    [#40] Update request model names

--- lib/model/request/submit_survey_request.dart
+++ lib/model/request/submit_survey_request.dart
@@ -1,12 +1,12 @@
 import 'package:json_annotation/json_annotation.dart';
-import 'package:lydiaryanfluttersurvey/model/request/submit_survey_question_request.dart';
+import 'package:lydiaryanfluttersurvey/model/request/question_request.dart';
 
 part 'submit_survey_request.g.dart';
 
 @JsonSerializable()
 class SubmitSurveyRequest {
   final String surveyId;
-  final List<SubmitSurveyQuestionRequest> questions;
+  final List<QuestionRequest> questions;
 
   SubmitSurveyRequest({
     required this.surveyId,
